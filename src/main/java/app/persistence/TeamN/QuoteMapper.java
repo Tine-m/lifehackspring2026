7976a21bb1a0e74e3b17a57c228ddaf944bd5b7b
GladVillainy <lucasbuades00@gmail.com>
2026-03-25 10:46:39 +0100

    Add QuoteMapper.
    Introduced QuoteMapper (src/main/java/app/persistence/TeamN/QuoteMapper.java). Does what a mapper does, but now with quotes.
    
    Not tested, so subject to be changed, thoughts and prayers for it to work.

---
+++ src/main/java/app/persistence/TeamN/QuoteMapper.java
@@ -0,0 +1,65 @@
+package app.persistence.TeamN;
+
+import app.entities.TeamN.Quote;
+import app.entities.login.User;
+import app.persistence.ConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuoteMapper {
+
+    public static List<Quote> getAllQuotes(ConnectionPool connectionPool) throws app.exceptions.common.DatabaseException {
+
+        List<Quote> quoteList = new ArrayList<>();
+
+        String sql = "select * from public.\"quotes\"";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String quote = rs.getString("quote");
+                int year = rs.getInt("year");
+                quoteList.add(new Quote(id, name, quote, year));
+            }
+        } catch (SQLException e) {
+            throw new app.exceptions.common.DatabaseException("DB fejl", e.getMessage());
+        }
+        return quoteList;
+    }
+
+    public static Quote getQuoteById(int id, ConnectionPool connectionPool) throws app.exceptions.common.DatabaseException {
+
+        Quote q = null;
+
+        String sql = "select * from public.\"quotes\" " +
+                "where id = ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                id = rs.getInt("id");
+                String name = rs.getString("name");
+                String quote = rs.getString("quote");
+                int year = rs.getInt("year");
+                q = new Quote(id, name, quote, year);
+            } else {
+                throw new app.exceptions.common.DatabaseException("Intet citat med dette ID fundet");
+            }
+        } catch (SQLException e) {
+            throw new app.exceptions.common.DatabaseException("DB fejl", e.getMessage());
+        }
+        return q;
+    }
+}
