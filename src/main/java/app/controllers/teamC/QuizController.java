9d9b52ab281db9b36d4d996fc68a9509812c30bd
stinetorndal <cph-sc95@stud.ek.dk>
2026-03-25 10:59:59 +0100

    html and QuizController added

--- src/main/java/app/controllers/teamC/QuizController.java
+++ src/main/java/app/controllers/teamC/QuizController.java
@@ -1,24 +1,26 @@
 package app.controllers.teamC;
 
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
 public class QuizController {
 
     /*statiske metoder gør koden sværere at teste, og de passer ikke så godt ind i
      * objektorienteret tankegang. Derfor er metoderne ikke statiske, og vi skal så huske
      * at oprette en instans af controlleren i Main.*/
 
-    public void addRoutes() {
+    public void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("startquiz", ctx -> showQuestion(ctx));
+        app.get("endquiz", ctx -> endQuiz(ctx));
+    }
+
+    public void showQuestion(Context ctx) {
 
     }
 
-    public void showQuestion() {
 
-    }
-
-    public void checkAnswer() {
-
-    }
-
-    public void leaveQuiz() {
+    public void endQuiz(Context ctx) {
 
     }
 }
