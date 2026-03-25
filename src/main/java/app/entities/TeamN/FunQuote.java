7b0ec83d31162b6a3ebe3783ae97147f1a326de8
GladVillainy <lucasbuades00@gmail.com>
2026-03-25 11:44:38 +0100

    Add FunQuote entity class
    Introduce FunQuote in app.entities.TeamN with id, text, and imagePath fields and a constructor.

---
+++ src/main/java/app/entities/TeamN/FunQuote.java
@@ -0,0 +1,15 @@
+package app.entities.TeamN;
+
+public class FunQuote {
+    private int id;
+    private String text;
+    private String imagePath;
+
+    public FunQuote(int id, String text, String imagePath) {
+        this.id = id;
+        this.text = text;
+        this.imagePath = imagePath;
+
+    }
+
+}
