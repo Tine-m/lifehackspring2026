3f1f1be190292af4c2d6773afdade1f87099d789
rasull9371 <rasullhossein250@gmail.com>
2026-03-25 19:16:51 +0100

    Created MovieMapper and methods

---
+++ src/main/java/app/persistence/teamO/MovieMapper.java
@@ -0,0 +1,56 @@
+package app.persistence.teamO;
+
+import app.entities.teamO.Movie;
+import app.exceptions.common.DatabaseException;
+import app.persistence.ConnectionPool;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieMapper {
+
+    public static List<Movie> getAllMovies(ConnectionPool connectionPool) throws DatabaseException {
+        List<Movie> movies = new ArrayList<>();
+        String sql = "SELECT film_id, title, year FROM teamO_films ORDER BY title";
+
+        try (Connection conn = connectionPool.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                movies.add(new Movie(
+                        rs.getInt("film_id"),
+                        rs.getString("title"),
+                        rs.getInt("year")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Fejl ved hentning af film", e.getMessage());
+        }
+        return movies;
+    }
+
+    public static Movie getMovieById(int movieId, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT film_id, title, year FROM teamO_films WHERE film_id = ?";
+
+        try (Connection conn = connectionPool.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, movieId);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return new Movie(
+                        rs.getInt("film_id"),
+                        rs.getString("title"),
+                        rs.getInt("year")
+                );
+            } else {
+                throw new DatabaseException("Film ikke fundet");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Fejl ved hentning af film", e.getMessage());
+        }
+    }
+}
