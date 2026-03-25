020150435b1b389309ba538cfdd94885c5b55252
KatarinaKN <katarinak.nielsen@gmail.com>
2026-03-25 12:58:55 +0100

    Quizcontroller: Added method body to showQuestion

--- src/main/java/app/controllers/teamC/QuizController.java
+++ src/main/java/app/controllers/teamC/QuizController.java
@@ -1,22 +1,39 @@
 package app.controllers.teamC;
 
+import app.entities.teamC.Question;
+import app.exceptions.common.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.teamC.QuizMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 public class QuizController {
+
+    QuizMapper quizMapper = new QuizMapper();
+    ConnectionPool connectionPool;
+
+    public QuizController(ConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
 
     /*statiske metoder gør koden sværere at teste, og de passer ikke så godt ind i
      * objektorienteret tankegang. Derfor er metoderne ikke statiske, og vi skal så huske
      * at oprette en instans af controlleren i Main.*/
 
     public void addRoutes(Javalin app, ConnectionPool connectionPool) {
-        app.get("startquiz", ctx -> showQuestion(ctx));
-        app.get("endquiz", ctx -> endQuiz(ctx));
+        app.get("/startquiz", ctx -> showQuestion(ctx));
+        app.get("/endquiz", ctx -> endQuiz(ctx));
     }
 
-    public void showQuestion(Context ctx) {
+    public Question showQuestion(Context ctx) {
+        try {
+            Question question = quizMapper.getRandomQuestion(connectionPool);
 
+        } catch (DatabaseException e) {
+            ctx.attribute("message", e.getMessage());
+            ctx.render("teamC/index.html");
+        }
+        return null;
     }
 
 
