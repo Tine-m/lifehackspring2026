90a4e3b1f943c2e0ef75b4988f3c8ad4302944ac
GladVillainy <lucasbuades00@gmail.com>
2026-03-26 12:29:35 +0100

    Add funQuotes table
    The table contains, funQuote_id (serial), funQuote_setup (text) and funQuote_punchLine(text).

---
+++ src/main/resources/sql/TeamN/funQuotes.sql
@@ -0,0 +1,17 @@
+CREATE TABLE funQuotes
+(
+    funQuote_id        SERIAL PRIMARY KEY,
+    funQuote_setup     TEXT NOT NULL,
+    funQuote_punchLine TEXT NOT NULL
+);
+
+INSERT INTO funQuotes (funQuote_setup, funQuote_punchLine)
+VALUES ('Hvem er historiens mindst sultne statsminister?', 'Mætte Frederiksen.'),
+       ('Hvad kalder man en ko uden ben?', 'Ground beef.'),
+       ('Hvad sagde manden da han gik ind i bussen?', 'Av!'),
+       ('Hvad gør en due, når den har travlt?', 'Den laver en to-do-liste.'),
+       ('Hvad kalder man en boomerang, der ikke vender tilbage?', 'En pind.'),
+       ('Hvorfor tager nordmænd altid bearnaisesovs med til fest?', 'Fordi der er estragon i den!'),
+       ('Min kammerat David mistede sit ID i dag.', 'Nu hedder han bare Dav'),
+       ('Nogle siger, at de ikke kan sove, når de drikker kaffe',
+        'Jeg har det lige omvendt. Jeg kan ikke drikke kaffe, når jeg sover.');
