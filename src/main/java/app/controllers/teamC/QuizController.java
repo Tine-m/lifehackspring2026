86a7021ff6d9de32464e54837d675b42e58a6c3d
KatarinaKN <katarinak.nielsen@gmail.com>
2026-03-26 10:31:05 +0100

    QuizController: deleted constructor, edited showQuestion() and endQuiz(), added backToMainPage().

--- src/main/java/app/controllers/teamC/QuizController.java
+++ src/main/java/app/controllers/teamC/QuizController.java
@@ -1,52 +1,56 @@
 package app.controllers.teamC;
 
 import app.entities.teamC.Question;
-import app.exceptions.common.DatabaseException;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.teamC.QuizMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.Collections;
 import java.util.List;
 
 public class QuizController {
 
-    QuizMapper quizMapper = new QuizMapper();
-    ConnectionPool connectionPool;
-
-    public QuizController(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    /*statiske metoder gør koden sværere at teste, og de passer ikke så godt ind i
-     * objektorienteret tankegang. Derfor er metoderne ikke statiske, og vi skal så huske
-     * at oprette en instans af controlleren i Main.*/
-
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/teamC", ctx -> index(ctx));
-        app.get("cirkusquiz", ctx -> showQuestion(ctx));
+        app.get("cirkusquiz", ctx -> showQuestion(ctx, connectionPool));
         app.get("endquiz", ctx -> endQuiz(ctx));
+        app.get("backtomainpage", ctx -> backToMainPage(ctx));
     }
 
     private static void index(Context ctx){
         ctx.render("teamC/index-quiz.html");
     }
 
-    public static void showQuestion(Context ctx)
-    {
-        try{
-            Question question = QuizMapper.getRandomQuestion(ConnectionPool.getInstance());
-            ctx.attribute("question", question);
+    public static void showQuestion(Context ctx, ConnectionPool connectionPool) {
+        try {
+            //Hent spørgsmål via mapperen
+            List<Question> questions = ctx.sessionAttribute("questions"); //Opretter en ny (tom) variabel og gemmer den i sessionen.
+
+            //Første gang er listen tom. Derfor griber vi nu listen fra getAllQuestions() og blander den herfter og gemmer den i sessionen.
+            if (questions == null || questions.isEmpty()) { //hvis listen ikke findes, eller den er tom.
+                questions = QuizMapper.getAllQuestions(connectionPool);
+                Collections.shuffle(questions);
+                ctx.sessionAttribute("questions", questions);
+            }
+
+            //Tag det første spørgsmål fra listen.
+            Question question = questions.get(0);
+            ctx.attribute("question", question); //Lægger et enkelt Question-objekt ned i en pakke, som Thymeleaf kan åbne i html.
+            ctx.render("teamC/cirkusquiz.html");
 
         } catch (DatabaseException e) {
             ctx.attribute("message", e.getMessage());
             ctx.render("teamC/index-quiz.html");
         }
-
     }
 
-
     public static void endQuiz(Context ctx) {
+        ctx.render("teamC/endquiz.html");
+    }
 
+    public static void backToMainPage(Context ctx) {
+        ctx.render("teamC/index-quiz.html");
     }
 }
