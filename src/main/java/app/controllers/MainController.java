fb3e0d1df548efac3d3788402f15170c013e42d8
MathiasStenvall <cph-ms1422@stud.ek.dk>
2026-03-26 08:54:34 +0100

    changed name to index.html

--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -19,6 +19,7 @@ public class MainController {
         List<LifehackSite> lifehackSites = new ArrayList<>();
 
         lifehackSites.add(new LifehackSite("Filosoffernes hjørne", "Få et råd", "/philosophers", "images/teamteachers/socrates.jpg"));
+        lifehackSites.add(new LifehackSite("Codle", "Spil", "/codle", "images/teamK/codle.png"));
         ctx.attribute("lifehackSites", lifehackSites);
         ctx.render("index.html");
     }
