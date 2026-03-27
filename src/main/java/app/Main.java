a0049ba8881e67129988408d383387075ecfc6be
Melissabekirova04 <229076304+Melissabekirova04@users.noreply.github.com>
2026-03-27 18:51:56 +0100

    Team B is new push

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 import app.config.ThymeleafConfig;
 import app.controllers.MainController;
+import app.controllers.teamB.TeamBQuoteController;
 import app.controllers.teamteachers.QuoteController;
 import app.controllers.login.UserController;
 import app.persistence.ConnectionPool;
@@ -15,7 +16,7 @@ public class Main
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "Lifehack";
+    private static final String DB = "lifehack";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
@@ -38,6 +39,8 @@ public class Main
 
         //Philosophers app - teamteachers
         QuoteController.addRoutes(app, connectionPool);
+        //team b
+        TeamBQuoteController.addRoutes(app, connectionPool);
 
     }
 }
