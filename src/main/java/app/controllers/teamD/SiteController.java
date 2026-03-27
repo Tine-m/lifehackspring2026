157aad4e246f127f9cf36e16cd9e4da0be3f2446
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2026-03-27 14:25:49 +0100

    removed unnecessary classes & data

--- src/main/java/app/controllers/teamD/SiteController.java
+++ src/main/java/app/controllers/teamD/SiteController.java
@@ -3,7 +3,6 @@ package app.controllers.teamD;
 import app.entities.teamD.Ingredient;
 import app.entities.teamD.Recipe;
 import app.persistence.ConnectionPool;
-import app.persistence.teamD.CsvImporter;
 import app.persistence.teamD.IngredientMapper;
 import app.persistence.teamD.RecipeMapper;
 import io.javalin.Javalin;
@@ -15,11 +14,6 @@ import java.util.*;
 public class SiteController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/teamD", ctx -> nemMadHomePage(ctx, connectionPool));
-        try {
-            CsvImporter.importCsv("src/main/resources/data/teamD/recipes_clean2.csv", "src/main/resources/data/teamD/ingredients_categorized.csv", connectionPool);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
     private static void nemMadHomePage(@NotNull Context ctx, ConnectionPool connectionPool) {
