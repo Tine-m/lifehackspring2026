5f1564ed23b812fc59a5cd9667c1342e3fe67420
BertramFM <160260021+BertramFM@users.noreply.github.com>
2026-03-27 12:34:31 +0100

    added basic filter + html rendering when adding ingredients

--- src/main/resources/public/js/teamD/functions.js
+++ src/main/resources/public/js/teamD/functions.js
@@ -12,7 +12,55 @@ document.querySelectorAll('.ingredient-item').forEach(item => {
             selectedIngredients.add(name);
             item.classList.add('selected');
         }
-
         countElement.textContent = selectedIngredients.size;
+        filter()
     });
 });
+
+
+function filter() {
+    if (selectedIngredients.size === 0){
+        renderRecipes(allRecipes)
+        return;
+    }
+
+    const filtered = allRecipes.filter(recipe =>
+        recipe.ingredients.some(i => selectedIngredients.has(i))
+    );
+
+    renderRecipes(filtered);
+
+}
+
+function renderRecipes(recipes) {
+    const container = document.querySelector('.section-block-grid');
+    document.getElementById('recipe-count').textContent = recipes.length;
+    document.getElementById('reccipe-count').textContent = recipes.length;
+
+    container.innerHTML = recipes.map(recipe => `
+        <div class="product-list-item">
+            <div class="product-item-image">
+                <img class="product-item-img" src="${recipe.imageUrl}">
+            </div>
+            <div class="product-item-text">
+                <div class="product-item-text-head">
+                    <h4 class="product-item-title">${recipe.title}</h4>
+                </div>
+                <div class="product-item-sub-text">
+                    <a href="${recipe.url}">valdemarsro.dk</a>
+                </div>
+                <div class="product-item-sub-text">
+                    retten tager <span>${recipe.totalTime}</span> min at lave
+                </div>
+                ${recipe.missing?.length > 0 ? `
+                    <div class="product-item-missing">
+                        Mangler: ${recipe.missing.join(', ')}
+                    </div>` : `
+                    <div class="product-item-complete"></div>`
+    }
+            </div>
+        </div>
+    `).join('');
+}
+
+renderRecipes(allRecipes);
