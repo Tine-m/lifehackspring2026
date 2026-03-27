f07ab55dd84c352d7b78f90dad914c88d010f5d4
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2026-03-27 12:49:38 +0100

    added search function for ingredients

--- src/main/resources/public/js/teamD/functions.js
+++ src/main/resources/public/js/teamD/functions.js
@@ -1,6 +1,9 @@
 const selectedIngredients = new Set();
 const countElement = document.getElementById("ingredient-count");
 
+const searchBar = document.querySelector(".head-searchbar");
+const searchInput = document.getElementById("search");
+
 document.querySelectorAll('.ingredient-item').forEach(item => {
     item.addEventListener('click', () => {
         const name = item.dataset.name.toLowerCase();
@@ -14,5 +17,36 @@ document.querySelectorAll('.ingredient-item').forEach(item => {
         }
 
         countElement.textContent = selectedIngredients.size;
+    });
+});
+
+
+
+searchBar.addEventListener("click", (e) => {
+    if (!e.target.closest(".search-button")) {
+        searchInput.focus();
+    }
+});
+
+const categoryBlocks = document.querySelectorAll('.ingredients-category-item');
+
+searchInput.addEventListener('input', () => {
+    const query = searchInput.value.trim().toLowerCase();
+
+    categoryBlocks.forEach(category => {
+        const items = category.querySelectorAll('.ingredient-item');
+        let anyVisible = false;
+
+        items.forEach(item => {
+            const name = item.dataset.name.toLowerCase();
+            if (name.includes(query)) {
+                item.style.display = '';
+                anyVisible = true;
+            } else {
+                item.style.display = 'none';
+            }
+        });
+
+        category.style.display = anyVisible ? '' : 'none';
     });
 });
